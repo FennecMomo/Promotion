d54d6e532040f77fc3f70868bc5d4039ab2fdb83
慕沐 <114895715+FennecMomo@users.noreply.github.com>
2026-02-13 22:44:30 +0800

    init

---
+++ Source/Promotion/Core/PromotionEntry.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+namespace Promotion
+{
+    [StaticConstructorOnStartup]
+    public static class PromotionEntry
+    {
+        static PromotionEntry()
+        {
+            var harmony = new Harmony("YourName.Promotion");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            
+            Log.Message("[晋升体系] Promotion v0.1.0 加载成功");
+        }
+    }
+}
